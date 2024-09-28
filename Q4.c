0836d55c9379f2fe58b5c2b8b91539a34adec050
Rayyan Ahmed <118850507+Rayyan9477@users.noreply.github.com>
2024-09-28 14:10:59 +0000

    tenth

--- Q4.c
+++ Q4.c
@@ -1,98 +1,66 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>  // Added for strcpy
 
-void create_process(const char* name) {
+void create_process(const char* name, int level);
+
+int main() {
+    printf("Root Process n (PID: %d)\n", getpid());
+    create_process("m", 0);
+    return 0;
+}
+
+void create_process(const char* name, int level) {
     pid_t pid = fork();
     
     if (pid < 0) {
-        // Fork failed
-        perror("fork failed");
+        perror("Fork failed");
+        exit(1);
     } else if (pid == 0) {
         // Child process
         printf("Process %s (PID: %d, Parent PID: %d)\n", name, getpid(), getppid());
+        
+        if (level < 4) {  // We don't create children for level 4 (leaf nodes)
+            if (level == 0) {
+                create_process("m", level + 1);
+                create_process("C2", level + 1);
+            } else if (level == 1) {
+                if (*name == 'm') {
+                    create_process("m", level + 1);
+                    create_process("C4", level + 1);
+                } else if (*name == 'C') {
+                    create_process("C1", level + 1);
+                    create_process("C3", level + 1);
+                }
+            } else if (level == 2) {
+                if (*name == 'm') {
+                    create_process("m", level + 1);
+                    create_process("C4", level + 1);
+                } else if (*name == 'C') {
+                    char child1[3], child2[3];
+                    snprintf(child1, sizeof(child1), "C%d", (*name - '0') * 2 - 1);
+                    snprintf(child2, sizeof(child2), "C%d", (*name - '0') * 2);
+                    create_process(child1, level + 1);
+                    create_process(child2, level + 1);
+                }
+            } else if (level == 3) {
+                char child1[3], child2[3];
+                if (*name == 'm') {
+                    strcpy(child1, "C0");
+                    strcpy(child2, "C1");
+                } else {
+                    snprintf(child1, sizeof(child1), "C%d", (*name - '0') * 2 - 1);
+                    snprintf(child2, sizeof(child2), "C%d", (*name - '0') * 2);
+                }
+                create_process(child1, level + 1);
+                create_process(child2, level + 1);
+            }
+        }
+        exit(0);
     } else {
-        // Parent process waits for the child to finish
-        wait(NULL);
+        // Parent process
+        wait(NULL);  // Wait for the child to terminate
     }
-}
-
-int main() {
-    pid_t pid;
-
-    // Root node 'n'
-    printf("Root Process n (PID: %d)\n", getpid());
-
-    // Create process m
-    pid = fork();
-    if (pid == 0) {
-        create_process("m");
-
-        // Create child processes under 'm'
-        pid_t pid2 = fork();
-        if (pid2 == 0) {
-            create_process("C0");
-        } else {
-            wait(NULL);
-            pid2 = fork();
-            if (pid2 == 0) {
-                create_process("C1");
-            } else {
-                wait(NULL);
-                pid2 = fork();
-                if (pid2 == 0) {
-                    create_process("C2");
-                } else {
-                    wait(NULL);
-                }
-            }
-        }
-    } else {
-        wait(NULL);
-        // Create process C1
-        pid = fork();
-        if (pid == 0) {
-            create_process("C1");
-
-            // Create child processes under 'C1'
-            pid_t pid3 = fork();
-            if (pid3 == 0) {
-                create_process("C3");
-            } else {
-                wait(NULL);
-                pid3 = fork();
-                if (pid3 == 0) {
-                    create_process("C4");
-                } else {
-                    wait(NULL);
-                }
-            }
-        } else {
-            wait(NULL);
-        }
-
-        // Create process C2
-        pid = fork();
-        if (pid == 0) {
-            create_process("C2");
-
-            // Create child processes under 'C2'
-            pid_t pid4 = fork();
-            if (pid4 == 0) {
-                create_process("C5");
-            } else {
-                wait(NULL);
-                pid4 = fork();
-                if (pid4 == 0) {
-                    create_process("C6");
-                } else {
-                    wait(NULL);
-                }
-            }
-        } else {
-            wait(NULL);
-        }
-    }
-
-    return 0;
 }
